06442e25b5069aec9ec6a2ba2f772da7c3ddb675
Mohammad <rasheed@live.com>
2018-08-14 22:29:43 +0530

    Log file addition

--- src/main/java/com/qa/utility/TestLog.java
+++ src/main/java/com/qa/utility/TestLog.java
@@ -1,5 +1,25 @@
 package com.qa.utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestLog {
 
+	File file;
+	FileInputStream fis;
+	Properties pro;
+	public void logFile(){
+		file=new File("C:\\Users\\Rasheed\\workspace\\framework\\src\\main\\java\\com\\qa\\config\\Log4j.properties");
+		try {
+			fis=new FileInputStream(file);
+			pro.load(fis);
+		} catch (FileNotFoundException e) {
+			e.getMessage();
+		} catch (IOException e) {
+			e.getMessage();
+		}
+	}
 }
